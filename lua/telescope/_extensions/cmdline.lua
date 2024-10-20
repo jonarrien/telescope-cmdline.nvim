8b05928ac1b9f2b772cedde891faa6669b0ec59a
Jon Arrien <jonarrien@gmail.com>
2024-10-20 13:02:50 +0200

    chore: readme improvements and cleanup (#27)

--- lua/telescope/_extensions/cmdline.lua
+++ lua/telescope/_extensions/cmdline.lua
@@ -2,6 +2,7 @@ local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
   error("This plugins requires nvim-telescope/telescope.nvim")
+  return
 end
 
 local cmdline = require('cmdline')
@@ -22,9 +23,9 @@ local displayer = entry_display.create({
 })
 
 local make_display = function(entry)
-  local config = assert(config.get(), "No config found")
+  local c = assert(config.get(), "No config found")
   return displayer({
-    { entry.icon, config.highlights.icon },
+    { entry.icon, c.highlights.icon },
     { entry.cmd },
   })
 end
@@ -44,19 +45,19 @@ local make_finder = function(config)
 end
 
 local make_picker = function(opts)
-  local config = assert(config.get(), "No config found")
-  return pickers.new(config.picker, {
+  local c = assert(config.get(), "No config found")
+  return pickers.new(c.picker, {
     prompt_title = "Cmdline",
     prompt_prefix = " : ",
-    finder = make_finder(config),
+    finder = make_finder(c),
     sorter = sorter(opts),
     attach_mappings = function(_, map)
       -- Autocomplete using <Tab>
-      map("i", config.mappings.complete, action.complete_input)
+      map("i", c.mappings.complete, action.complete_input)
       -- Run selection with <CR>
-      map("i", config.mappings.run_selection, action.run_selection)
+      map("i", c.mappings.run_selection, action.run_selection)
       -- Run command from input field with <C-CR> (special cases) ??
-      map("i", config.mappings.run_input, action.run_input)
+      map("i", c.mappings.run_input, action.run_input)
       map("i", "<C-e>", action.edit)
 
       require("telescope.actions").close:enhance {
@@ -82,13 +83,18 @@ local cmdline_visual = function(opts)
   picker:set_prompt("'<,'> ")
 end
 
+-- Telescope extension setup
+-- NOTE: It's loads the extension twice (https://github.com/nvim-telescope/telescope.nvim/issues/2659)
+local setup = function(ext_config, user_config)
+  if vim.fn.has('nvim-0.10') == 0 then
+    vim.notify('Cmdline requires Neovim 0.10.0 or higher', vim.log.levels.ERROR, {})
+  end
+
+  require("cmdline.config").set_defaults(ext_config)
+end
+
 return telescope.register_extension({
-  setup   = function(ext_config, user_config)
-    if vim.fn.has('nvim-0.10') == 0 then
-      vim.notify('Cmdline extension requires Neovim 0.10 or higher', vim.log.levels.ERROR, {})
-    end
-    require("cmdline.config").set_defaults(ext_config)
-  end,
+  setup   = setup,
   exports = {
     cmdline = telescope_cmdline,
     visual  = cmdline_visual,
