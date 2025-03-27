9ab5cb75b81db59041a8042ca0b2912c58950f06
Marcus Faulstich <marcus.faulstich@tu-ilmenau.de>
2025-03-27 11:16:05 +0100

    fixed typo in comment

--- lua/telescope/_extensions/cmdline.lua
+++ lua/telescope/_extensions/cmdline.lua
@@ -59,7 +59,7 @@ local make_picker = function(opts)
       -- Run command from input field with <C-CR> (special cases) ??
       map("i", c.mappings.run_input, action.run_input)
       map("i", "<C-e>", action.edit)
-      -- Close the prompt with <C-e>
+      -- Close the prompt with <C-c>
       map("i", c.mappings.close, action.close)
 
       require("telescope.actions").close:enhance {
